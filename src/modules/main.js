1389cff3eae1c0ab273c6eba27a1e720652c8cb3
Marcos Rodrigues <marcosleo014@gmail.com>
2026-06-26 16:52:01 -0300

    criação da função que alterna entre lista e mensagem de lista vazia

--- src/modules/main.js
+++ src/modules/main.js
@@ -3,3 +3,4 @@ import '../styles/index.css';
 
 // JavaScript
 import './switch-screen.js'
+import './verify-empty-msg.js'
---
+++ src/modules/verify-empty-msg.js
@@ -0,0 +1,15 @@
+function verifyEmptyMsg() {
+    const scheduleListsDOM = document.querySelectorAll('.schedule-list')
+    scheduleListsDOM.forEach((list) => {
+        const emptyMsg = list.previousElementSibling;
+        if (list.children.length == 0) {
+            list.hidden = true;
+            emptyMsg.hidden = false;
+        } else {
+            list.hidden = false;
+            emptyMsg.hidden = true;
+        };
+    });
+};
+
+verifyEmptyMsg();
